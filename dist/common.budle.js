3852850bcb64f8268a945ee0e58cf50964d0ae27
guogege <943522133@qq.com>
2019-03-13 22:46:30 +0800

    webpack公共代码提取

---
+++ dist/common.budle.js
@@ -0,0 +1,27 @@
+webpackJsonp([3],[
+/* 0 */,
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__module__ = __webpack_require__(4);
+
+/* unused harmony default export */ var _unused_webpack_default_export = ('subPageA');
+
+/***/ }),
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* unused harmony default export */ var _unused_webpack_default_export = ('subPageB');
+
+/***/ }),
+/* 3 */,
+/* 4 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* unused harmony default export */ var _unused_webpack_default_export = ('moduleAAA');
+
+/***/ })
+]);
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack')
+var path = require('path')
+
+module.exports = {
+    entry: {
+        'pageA': './src/pageA',
+        'pageB': './src/pageB',
+        'vendor': ['lodash']
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].budle.js',
+        chunkFilename:  '[name].chunk.js'
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor', 'manifest'],
+            minChunks: Infinity
+        }),
+
+        // new webpack.optimize.CommonsChunkPlugin({
+        //     name: 'manifest',
+        //     minChunks: Infinity
+        // }),
+
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            minChunks: 2,
+            chunks: ['pageA', 'pageB']
+        }),
+    ]
+}
